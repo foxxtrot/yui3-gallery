cf7738ae7d88bd446caa3c8c7e5aff0c11c3c966
Jeff Craig <foxxtrot@foxxtrot.net>
2009-11-20 14:05:48 -0800

    Converted to be a plugin for Node.
    It checks to make sure that the host is a Node instance which is also a password field.

--- src/gallery-chromahash/js/chromahash.js
+++ src/gallery-chromahash/js/chromahash.js
@@ -7,8 +7,11 @@ var LBL_TMPL = '<label for="{id}" class="{color} chroma-hash"></label>',
 		_C = function(conf) { 
 			_C.superclass.constructor.apply(this, arguments);
 			this._animations = [];
+			this.renderUI();
+			this.bindUI();
 		};
 _C.NAME = "ChromaHash";
+_C.NS = "ChromaHash";
 _C.ATTRS = 
 	{
 		bars: {
@@ -25,53 +28,58 @@ _C.ATTRS =
 		minimum: {
 			value: 6
 		},
-		node: {
+		host: {
 			setter: function(node) {
 				var n = Y.get(node);
 				if (!n && !n.test('input[type=password]')) {
 					Y.fail('ChromaHash: Invalid node provided: ' + node);
 				}
+				this._lblQry = 'label[for=' + n.get('name') + '].chroma-hash';
 				return n;
 			}
 		}
  };
-Y.extend(_C, Y.Widget, {
+Y.extend(_C, Y.Plugin.Base, {
 	renderUI: function() {
-		var colors = ["primary", "secondary", "tertiary", "quaternary"].slice(0, this.get('bars')),
-			c = this.get('contentBox'), n = this.get('node'), i, lbl, width = n.get('clientWidth'),
-			height = n.get('clientHeight'), position = n.getXY();
-		// Preferably, I'd be able to set the position on the boudningBox, but for now, this will function.
-		for (i = 0 ; i < colors.length ; i += 1) {
-			lbl = Y.Node.create(LBL_TMPL.replace(/\{id\}/g, n.get('id')).replace(/\{color\}/g, colors[i]));
-			lbl.setStyles({
-				position: 'absolute',
-				height: height + "px",
-				width: "8px",
-				margin: "5px",
-				marginLeft: "0px",
-				backgroundColor: this.get('node').getStyle('backgroundColor')
-			});
-			c.insert(lbl);
-			lbl.setXY([position[0] + width - 2 + (-8 * (i + 1)), position[1] + 3]);
-			this._animations.push(new Y.Anim({node: lbl, duration: 0.5}));
+		if (!this._rendered) {
+			var colors = ["primary", "secondary", "tertiary", "quaternary"].slice(0, this.get('bars')),
+				c = Y.get('body'), n = this.get('host'), i, lbl, width = n.get('clientWidth'),
+				height = n.get('clientHeight'), position = n.getXY();
+			// Preferably, I'd be able to set the position on the boudningBox, but for now, this will function.
+			for (i = 0 ; i < colors.length ; i += 1) {
+				lbl = Y.Node.create(LBL_TMPL.replace(/\{id\}/g, n.get('id')).replace(/\{color\}/g, colors[i]));
+				lbl.setStyles({
+					position: 'absolute',
+					height: height + "px",
+					width: "8px",
+					margin: "5px",
+					marginLeft: "0px",
+					backgroundColor: n.getStyle('backgroundColor')
+				});
+				c.insert(lbl);
+				lbl.setXY([position[0] + width - 2 + (-8 * (i + 1)), position[1] + 3]);
+				this._animations.push(new Y.Anim({node: lbl, duration: 0.5}));
+			}
+			this._rendered = true;
 		}
 	},
 	bindUI: function() {
-		this._keyHandler = this.get('node').after('keyup', this._handleKey, this);
+		this._keyHandler = this.get('host').after('keyup', this._handleKey, this);
 	},
 	hide: function() {
-		this.get('boundingBox').setStyle('display','none');
+		Y.all(this._lblQry).setStyle('display','none');
 	},
 	clear: function() {
 		this.hide();
-		this.get('boundingBox').set('innerHTML','');
+		Y.all(this._lblQry).remove();
+		this._rendered = false;
 	},
 	destroy: function() {
 		this._keyHandler.detach();
 		this._clear();
 	},
 	_handleKey: function(e) {
-		var n = this.get('node'), value = n.get('value'), i,
+		var n = this.get('host'), value = n.get('value'), i,
 				col = [], bars = this.get('bars'), md5, g;
 		if(value === "" ) {
 			for( i = 0 ; i < bars ; i += 1) { col.push('fff'); }
