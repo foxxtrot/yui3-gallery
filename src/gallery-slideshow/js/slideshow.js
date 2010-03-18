28f8881a04954e13e4a3a06b0d6fe16ecc97c8f9
Jeff Craig <foxxtrot@foxxtrot.net>
2010-03-18 07:20:48 -0700

    Move imageList progressive enhancement out of renderUI.

--- src/gallery-slideshow/js/slideshow.js
+++ src/gallery-slideshow/js/slideshow.js
@@ -1,5 +1,7 @@
 var _S = function() {
+	this._imageList = [];
 	_S.superclass.constructor.apply(this, arguments);
+
 }, 
 	SLIDESHOW = "SlideShow", 
 	ISNUMBER = Y.Lang.isNumber;
@@ -22,7 +24,14 @@ _S.HTML_PARSER =
 		},
 		bodyNode: ".yui3-widget-body",
 		headerNode: ".yui3-widget-header",
-		footerNode: ".yui3-widget-footer"
+		footerNode: ".yui3-widget-footer",
+		images: function(contentBox) {
+			contentBox.all('.yui3-widget-body li img').each(function(node) {
+				var img = {};
+				this._parseImage(node, img);
+				this._imageList.push(img);
+			}, this);
+		}
 	};
 _S.ATTRS = 
 	{
@@ -31,8 +40,15 @@ _S.ATTRS =
 				validator: ISNUMBER
 			},
 		images: {
-				value: [],
-				validator: Y.Lang.isArray
+				validator: Y.Lang.isArray,
+				setter: function(value) {
+					Y.Array.each(value, function(v) {
+						this._imageList.push(v);
+					}, this);
+				},
+				getter: function() {
+					return this._imageList;
+				}
 			},
 		animation: {
 				validator: Y.Lang.isObject,
@@ -116,11 +132,8 @@ Y.extend(_S, Y.Widget,
 			this.get('contentBox').insert(node, where);
 			return node;
 		},
-		_parseImage: function(imgNode) {
-			var img = {}, images = this.get('images');
-			img.src = imgNode.get('src');
-			images.unshift(img);
-			this.set('images', images);
+		_parseImage: function(imgNode, imgObj) {
+			imgObj.src = imgNode.get('src');
 		},
 		renderUI: function() {
 			var images = this.get('images'), bodyNode = this.get('bodyNode'), title = this.get('title'), image_height = this.get('image_height'), image_width = this.get('image_width');
@@ -132,10 +145,7 @@ Y.extend(_S, Y.Widget,
 			}}
 			if (image_width) { bodyNode.setStyle('width', image_width); }
 			if (image_height) { bodyNode.setStyle('height', image_height); }
-			bodyNode.all('li').each(function(node, index, nodeList) {
-				this._parseImage(node.one('img'));
-				node.remove();
-			}, this);
+			bodyNode.all('li').remove();
 			Y.Array.each(images, function(i, d, a) {
 				var x = this.createImage(i, -1*d);
 				if (d === 0) { this.currentImage = x; }
