89d0b9d0560952bb32df9ba7c9347a59fbae4508
Jeff Craig <foxxtrot@foxxtrot.net>
2010-12-24 14:22:41 -0800

    Added YUIDoc comments

--- src/gallery-nodelist-extras/js/nodelist-extras.js
+++ src/gallery-nodelist-extras/js/nodelist-extras.js
@@ -1,6 +1,12 @@
 var NodeListPrototype = Y.NodeList.prototype,
     _selectorFilter = NodeListPrototype.filter;
 
+/**
+ * Filters the NodeList instance to only the matching nodes
+ * @method Y.NodeList.filter
+ * @param {string|function} filter A CSS selector string to apply to the list, or a boolean function to apply to each element in the list. The function will be passed a single argument, a Y.Node instance of the current node.
+ * @return {NodeList} NodeList containing the updated collection
+ */
 NodeListPrototype.filter = function(filter) {
     var newList;
     if (typeof(filter) === "string") {
