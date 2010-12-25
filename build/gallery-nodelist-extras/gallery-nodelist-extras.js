aa4ed3d6616d77e1a50457662810ab6055815143
Jeff Craig <foxxtrot@foxxtrot.net>
2010-12-24 19:22:08 -0800

    Add reject method, clean up doc comments

--- build/gallery-nodelist-extras/gallery-nodelist-extras.js
+++ build/gallery-nodelist-extras/gallery-nodelist-extras.js
@@ -3,6 +3,15 @@ YUI.add('gallery-nodelist-extras', function(Y) {
 var NodeListPrototype = Y.NodeList.prototype,
     _selectorFilter = NodeListPrototype.filter;
 
+/**
+ * Filters the NodeList instance to only the matching nodes
+ * @method Y.NodeList.filter
+ * @param {string|function} filter A CSS selector string to apply to the list,
+ * or a boolean function to apply to each element in the list. The function
+ * will be passed three arguments, a Y.Node instance of the current node, the
+ * current index in the list, and the NodeList instance.
+ * @return {NodeList} NodeList containing the updated collection
+ */
 NodeListPrototype.filter = function(filter) {
     var newList;
     if (typeof(filter) === "string") {
@@ -10,12 +19,35 @@ NodeListPrototype.filter = function(filter) {
     }
     if (typeof(filter) === 'function') {
         newList = new Y.NodeList([]);
-        this.each(function(node) {
-            if (filter(node)) {
+        this.each(function(node, index, list) {
+            if (filter(node, index, list)) {
                 newList.push(node);
             }
         });
         return newList;
+    }
+
+    return this;
+};
+/**
+ * This inverse of filter. Applies :not to a selector, or builds a NodeList
+ * consisting of all the nodes for which the argument is false
+ * @param {string|function} filter A CSS selector string to apply to the list,
+ * or a boolean function to apply to each element in the list. The function
+ * will be passed three arguments, a Y.Node instance of the current node, the
+ * current index in the list, and the NodeList instance.
+ * @return {NodeList} NodeList containing the updated collection
+ */
+NodeListPrototype.reject = function(filter) {
+    if (typeof(filter) === "string") {
+        return this.filter(function(item) {
+                    return !item.test(filter);
+                });
+    }
+    if (typeof(filter) === "function") {
+        return this.filter(function(item, i, a) {
+                    return !filter.call(null, item, i, a);
+                });
     }
 
     return this;
--- src/gallery-nodelist-extras/js/nodelist-extras.js
+++ src/gallery-nodelist-extras/js/nodelist-extras.js
@@ -4,7 +4,10 @@ var NodeListPrototype = Y.NodeList.prototype,
 /**
  * Filters the NodeList instance to only the matching nodes
  * @method Y.NodeList.filter
- * @param {string|function} filter A CSS selector string to apply to the list, or a boolean function to apply to each element in the list. The function will be passed three arguments, a Y.Node instance of the current node, the current index in the list, and the NodeList instance.
+ * @param {string|function} filter A CSS selector string to apply to the list,
+ * or a boolean function to apply to each element in the list. The function
+ * will be passed three arguments, a Y.Node instance of the current node, the
+ * current index in the list, and the NodeList instance.
  * @return {NodeList} NodeList containing the updated collection
  */
 NodeListPrototype.filter = function(filter) {
@@ -22,6 +25,30 @@ NodeListPrototype.filter = function(filter) {
         return newList;
     }
 
-    Y.log("Unrecognized filter argument, returning list");
+    Y.log("Unrecognized filter type, returning list");
+    return this;
+};
+/**
+ * This inverse of filter. Applies :not to a selector, or builds a NodeList
+ * consisting of all the nodes for which the argument is false
+ * @param {string|function} filter A CSS selector string to apply to the list,
+ * or a boolean function to apply to each element in the list. The function
+ * will be passed three arguments, a Y.Node instance of the current node, the
+ * current index in the list, and the NodeList instance.
+ * @return {NodeList} NodeList containing the updated collection
+ */
+NodeListPrototype.reject = function(filter) {
+    if (typeof(filter) === "string") {
+        return this.filter(function(item) {
+                    return !item.test(filter);
+                });
+    }
+    if (typeof(filter) === "function") {
+        return this.filter(function(item, i, a) {
+                    return !filter.call(null, item, i, a);
+                });
+    }
+
+    Y.log("Unrecognized filter type, returning list");
     return this;
 };
