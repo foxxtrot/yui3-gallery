84ee16beddb757d0b41cde0377ba51fb68549dd9
Jeff Craig <foxxtrot@foxxtrot.net>
2010-12-24 18:48:21 -0800

    Filter will now pass the index and NodeList to the function.
    This brings the API more in spec with the Y.Array.filter method and
    Y.NodeList.each

--- src/gallery-nodelist-extras/js/nodelist-extras.js
+++ src/gallery-nodelist-extras/js/nodelist-extras.js
@@ -4,7 +4,7 @@ var NodeListPrototype = Y.NodeList.prototype,
 /**
  * Filters the NodeList instance to only the matching nodes
  * @method Y.NodeList.filter
- * @param {string|function} filter A CSS selector string to apply to the list, or a boolean function to apply to each element in the list. The function will be passed a single argument, a Y.Node instance of the current node.
+ * @param {string|function} filter A CSS selector string to apply to the list, or a boolean function to apply to each element in the list. The function will be passed three arguments, a Y.Node instance of the current node, the current index in the list, and the NodeList instance.
  * @return {NodeList} NodeList containing the updated collection
  */
 NodeListPrototype.filter = function(filter) {
@@ -14,8 +14,8 @@ NodeListPrototype.filter = function(filter) {
     }
     if (typeof(filter) === 'function') {
         newList = new Y.NodeList([]);
-        this.each(function(node) {
-            if (filter(node)) {
+        this.each(function(node, index, list) {
+            if (filter(node, index, list)) {
                 newList.push(node);
             }
         });
